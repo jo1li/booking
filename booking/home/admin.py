3fbb239273cdef244803728fd2c54e58e49250d1
Chris Henry <henry.christopher@gmail.com>
2018-03-31 20:16:53 -0400

    new: Admin for OpusUser object

--- booking/home/admin.py
+++ booking/home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from home.models import OpusUser
+
+class OpusUserAdmin(admin.ModelAdmin):
+    pass
+
 # Register your models here.
+admin.site.register(OpusUser, OpusUserAdmin)
