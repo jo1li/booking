d91efd6a47b23a7b09be8c6065753337b95883b8
Chris Henry <henry.christopher@gmail.com>
2018-04-19 17:53:49 -0400

    chg: Refactor instagram followers to live in user model

--- booking/home/models.py
+++ booking/home/models.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+import requests
 
 class OpusUser(AbstractUser):
     is_musician = models.BooleanField('musician status', default=False)
     is_booking_agent = models.BooleanField('booking agent status', default=False)
+
+    def instagram_followers(self):
+        insta_auth = self.social_auth.get(provider='instagram')
+
+        insta_url = "https://api.instagram.com/v1/users/self/?access_token={}".format(insta_auth.extra_data['access_token'])
+        r = requests.get(insta_url)
+
+        return r.json()['data']['counts']['followed_by']
