34708dd66489837fb4eaabebd0ac22c693d742be
Zak Stallings <zakstal@gmail.com>
2018-05-31 08:55:35 -0700

    add vue example

---
+++ booking/static/js/vue/src/App.js
@@ -0,0 +1,23 @@
+// import Vue from 'vue';
+// import withRender from '../htmlTemplate/main.html';
+
+// export default withRender({
+//   name: 'app',
+//   data () {
+//     return {
+//       msg: 'test',
+//       yes: true,
+//       count: 0,
+//     }
+//   },
+//   computed: {
+//     superduper: function () {
+//       return this.count * 3;
+//     }
+//   },
+//   watch: {
+//     count: function (va) {
+//       console.log(va)
+//     }
+//   }
+// })
