ee11233b4c4d0daec6a75f4be83a66e5410183de
Chris Henry <henry.christopher@gmail.com>
2018-06-06 18:20:09 -0400

    chg: Rejigger some paths

--- Dockerfile.js
+++ Dockerfile.js
@@ -2,9 +2,9 @@ FROM node:8.11.2-alpine
 
 RUN mkdir -p /app/
 COPY ./booking/static/js/vue /app/static/js/vue
-WORKDIR /app/
+WORKDIR /app/static/js/vue
 
-RUN npm install --prefix static/js/vue
-RUN npm run build --prefix static/js/vue
+RUN npm install
+RUN npm run build
 
 CMD ["/usr/local/bin/npm run build"]
