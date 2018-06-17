a305cb0f1fc82a4a09c86235e7ec7750e77310b2
Chris Henry <henry.christopher@gmail.com>
2018-06-17 16:32:49 -0400

    chg: Put back the transaction case

--- booking/musicians/tests/utils.py
+++ booking/musicians/tests/utils.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 
 from musicians.tests.mommy_recipes import musician_recipe, admin_user_recipe
 
-class OpusTestCase(WebTest):
+class OpusTestCase(WebTest, TransactionTestCase):
 
 
     def setUp(self):
