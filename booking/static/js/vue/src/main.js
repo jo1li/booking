db8ede72d584dde5449a6e497d02dc0318c6ac9c
KC Soderstrom <soderstromkc@gmail.com>
2018-06-20 22:49:12 -0700

    Add video carousel to musicain profile page, with random stock video

--- booking/static/js/vue/src/main.js
+++ booking/static/js/vue/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.js'
 import VueFormGenerator from "vue-form-generator";
 import UserEditForm from './components/user-edit-form.vue'
+import VideoCarousel from './components/video-carousel.vue'
 import Vuetify from 'vuetify'
 var VueCookie = require('vue-cookie');
 
@@ -21,7 +22,8 @@ new Vue({
     editBioOpen: true,
   },
   components: {
-    UserEditForm
+    UserEditForm,
+    VideoCarousel,
   },
   methods: {
     openEditBio: function () {
