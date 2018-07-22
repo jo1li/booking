f2d4e2c4709f46a5cdb64804fb423bbd46fc6a07
KC Soderstrom <soderstromkc@gmail.com>
2018-07-21 22:50:12 -0500

    Add CRUD actions for musician videos

---
+++ booking/static/js/app/src/actions/videos.js
@@ -0,0 +1,30 @@
+import ActionCreators from './ActionCreators';
+import * as requests from '../request/requests';
+import crudConfigs from '../reducers/crudConfigs';
+
+// TODO: error handling
+
+export function createMusicianVideo({musicianId, code}) {
+    return (dispatch, getState) => {
+        const request = requests.createVideo({musicianId, code}).then(res => {
+            dispatch(ActionCreators.videosCreateOrUpdate(res.data));
+        });
+        return request;
+    }
+}
+
+export function updateMusicianVideo({musicianId, videoId, code}) {
+    return (dispatch, getState) => {
+        const request = requests.updateVideo({musicianId, videoId, code}).then(res => {
+            dispatch(ActionCreators.videosCreateOrUpdate(res.data.results));
+        })
+    }
+}
+
+export function getMusicianVideos({musicianId}) {
+    return (dispatch, getState) => {
+        const request = requests.getVideos({musicianId}).then(res => {
+            dispatch(ActionCreators.videosCreateOrUpdate(res.data.results));
+        });
+    }
+}
