a629bd6ee6042583a45921728645d81eed70f3f7
KC Soderstrom <soderstromkc@gmail.com>
2018-07-21 22:59:09 -0500

    Bind video edit form to DOM

--- booking/static/js/app/src/index.js
+++ booking/static/js/app/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './index.css';
 import VideoCarousel from './components/VideoCarousel';
 import UserEditForm from './components/UserEditForm';
+import VideoEditForm from './components/VideoEditForm';
 import RenderFromDomNode from './renderFromDomNode';
 
 RenderFromDomNode({
     Component: UserEditForm,
     node: 'user-edit-form',
+})
+
+RenderFromDomNode({
+    Component: VideoEditForm,
+    node: 'video-edit-form',
 })
 
 RenderFromDomNode({
