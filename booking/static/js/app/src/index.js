72b06ee96ccb7fa899a0c2c0b0c809f16f894c32
KC Soderstrom <soderstromkc@gmail.com>
2018-07-23 11:24:51 -0500

    Bootstrap images base url into CONFIGS object to contain bootstrapped config values

--- booking/static/js/app/src/index.js
+++ booking/static/js/app/src/index.js
@@ -4,12 +4,16 @@ import VideoCarousel from './components/VideoCarousel';
 import UserEditForm from './components/UserEditForm';
 import RenderFromDomNode from './renderFromDomNode';
 
+const CONFIGS = window.CONFIGS || {};
+
 RenderFromDomNode({
     Component: UserEditForm,
     node: 'user-edit-form',
 })
 
+// TODO: Use `CONFIGS` to set the image base url (in other PR)
 RenderFromDomNode({
     Component: VideoCarousel,
     node: 'video-carousel',
+    CONFIGS,
 })
