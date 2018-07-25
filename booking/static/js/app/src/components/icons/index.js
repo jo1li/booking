c17c9fb7727334819e91f9ce86863bde72616dd6
KC Soderstrom <soderstromkc@gmail.com>
2018-07-25 11:41:10 -0500

    Use bootstrapped configs for images url in icons

--- booking/static/js/app/src/components/icons/index.js
+++ booking/static/js/app/src/components/icons/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IconImg from './IconImg';
+import CONFIGS from '../../configs';
 
 export { default as CloudUpload } from '@material-ui/icons/CloudUpload';
 export { default as Delete } from '@material-ui/icons/Delete';
@@ -9,4 +10,4 @@ export { default as Add } from '@material-ui/icons/Add';
 export { default as ExpandMore } from '@material-ui/icons/ExpandMore';
 export { default as CheckCircle } from '@material-ui/icons/CheckCircle';
 
-export const Save = IconImg('/static/images/save.svg')
+export const Save = IconImg(`${CONFIGS.IMAGES_URL}/save.svg`)
