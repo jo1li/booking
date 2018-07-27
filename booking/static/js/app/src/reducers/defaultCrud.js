ef142bd03f191cc156196bdb4fd15bdb27390878
Zak Stallings <zakstal@gmail.com>
2018-07-26 20:22:34 -0700

    remove docs

--- booking/static/js/app/src/reducers/defaultCrud.js
+++ booking/static/js/app/src/reducers/defaultCrud.js
@@ -9,34 +9,7 @@ import {
   _CREATE_OR_UPDATE
 } from '../constants/actionPartials';
 
-/**
- * For Immutable.orderedMap();
- * @param reducerName
- * @returns {{}}
- *
- * The purpose of defaultCrud is to make generic collection creation, update and deletion
- * for a slice of state really easy to spin up.
- *
- * given a reducerName default CRUD actions will be created.
- * For example given a reducerName of 'todos' you will get actions:
- *
- * 1. TODOS_CREATE_OR_UPDATE
- * 2. TODOS_DELETE
- *
- * dispatches to TODOS_CREATE_OR_UPDATE can accept:
- *
- * 1. an array of records where each record has an id. This will create records in state.
- * 2. an object with an id. this will create or update a record in state
- * 3. an an object without an id. this will create or update an object in state
- *
- * dispatches to TODOS_DELETE can accept:
- *
- * 1. an array of objects with id. this will delete those records in state.
- * 2. an object with an id. this will delete that record.
- * 3. passing nothing will reset all records.
- *
- *
- */
+
 const defaultCrud = function (reducerName, configs) {
   const key = (configs && configs.key) || 'id';
   const initialState = {};
