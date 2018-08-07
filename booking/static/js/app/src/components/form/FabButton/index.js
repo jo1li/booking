dfc8bd9641d593a154b9254382c179cc0528acae
KC Soderstrom <soderstromkc@gmail.com>
2018-08-07 18:09:09 -0500

    Add move and help buttons, with temporary icons

--- booking/static/js/app/src/components/form/FabButton/index.js
+++ booking/static/js/app/src/components/form/FabButton/index.js
@@ -1,11 +1,15 @@
 import {
+    Add,
     CloudUpload,
     Delete,
-    Add,
+    Help,
+    Move,
 } from '../../icons';
 import FabIconButton from './FabIconButton';
 
 export { default } from './FabButton';
+export const AddButton = FabIconButton(Add);
 export const UploadButton = FabIconButton(CloudUpload);
 export const DeleteButton = FabIconButton(Delete);
-export const AddButton = FabIconButton(Add);
+export const HelpButton = FabIconButton(Help);
+export const MoveButton = FabIconButton(Move);
--- booking/static/js/app/src/components/icons/index.js
+++ booking/static/js/app/src/components/icons/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import IconImg from './IconImg';
 
+export { default as Add } from '@material-ui/icons/Add';
+export { default as CheckCircle } from '@material-ui/icons/CheckCircle';
+export { default as Close } from '@material-ui/icons/Close';
 export { default as CloudUpload } from '@material-ui/icons/CloudUpload';
 export { default as Delete } from '@material-ui/icons/Delete';
-export { default as Close } from '@material-ui/icons/Close';
-// export { default as Save } from '@material-ui/icons/Save';
-export { default as Add } from '@material-ui/icons/Add';
 export { default as ExpandMore } from '@material-ui/icons/ExpandMore';
-export { default as CheckCircle } from '@material-ui/icons/CheckCircle';
-
+// TODO: Change these to be the icons we have in zeplin
+export { default as Help } from '@material-ui/icons/HelpOutline';
+export { default as Move } from '@material-ui/icons/OpenWith';
 export const Save = IconImg('/static/images/save.svg')
