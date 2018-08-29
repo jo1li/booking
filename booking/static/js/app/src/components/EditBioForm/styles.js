5664dca7201181f0713d1ababc2b69c7c7477486
kcsoderstrom <kcsoderstrom@users.noreply.github.com>
2018-08-29 16:26:05 -0500

    Make cancel confirm not higher order (#54)

--- booking/static/js/app/src/components/EditBioForm/styles.js
+++ booking/static/js/app/src/components/EditBioForm/styles.js
@@ -1,4 +1,22 @@
+// TODO: Share more of these styles between the modals
 const styles = theme => ({
+  container: {
+    maxWidth: '100%',
+    width: '100%',
+    height: '100%',
+    padding: '20px',
+    paddingTop: '40px',
+    paddingBottom: '40px',
+  },
+  caption: {
+    color: theme.palette.grey['A400'],
+  },
+  captionTop: {
+    margin: '0 12px',
+    padding: '12px 0px 5px!important',
+    color: theme.palette.grey['A400'],
+    borderBottom: `1px solid ${theme.palette.grey[200]}`,
+  },
   textArea: {
     height: '300px',
   },
