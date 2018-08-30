45d2d337afea104c545772bc8c84f3bc277aa24d
kcsoderstrom <kcsoderstrom@users.noreply.github.com>
2018-08-30 15:46:47 -0500

    Add help tab to video edit form (#48)
    # Changelog
    
    For [this trello card](https://trello.com/c/6VzKSnPn/78-profile-edit-video-followups).
    
    This PR adds a "help" tab to the video edit form to instruct users on what to enter in the "code" field.
    
    Remaining items documented [in slack](https://opusapp.slack.com/archives/G9Q3R3HPC/p1535149568000100).
    
    ## Changes
    * Make `CancelConfirm` a normal component that contains only the cancel and confirm buttons, not a higher-order container component.
    
    ## New
    * Add tabs to the video edit form, with a new "help tab".
    
    https://github.com/OpusApps/booking/pull/48

--- booking/static/js/app/src/components/UserEditForm/styles.js
+++ booking/static/js/app/src/components/UserEditForm/styles.js
@@ -25,11 +25,6 @@ const styles = theme => ({
       marginLeft: '0px',
     }
   },
-  captionTop: {
-      margin: '0 12px',
-      padding: '12px 0px 5px!important',
-      borderBottom: `1px solid ${theme.palette.grey[200]}`,
-  },
 });
 
 export default styles;
