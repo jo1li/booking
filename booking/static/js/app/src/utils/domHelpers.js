ccb73ac327e5e18d80f9d128704295236cc1edb4
Chris Henry <henry.christopher@gmail.com>
2018-09-17 16:24:32 -0400

    chg: a little formatting love

--- booking/static/js/app/src/utils/domHelpers.js
+++ booking/static/js/app/src/utils/domHelpers.js
@@ -1,12 +1,15 @@
 import {forEach} from 'lodash';
 
 export function bindDomEvent(domSelector, eventType, callback) {
-     const domElements = document.querySelectorAll(domSelector);
-     if(domElements.length === 0) {
+
+  const domElements = document.querySelectorAll(domSelector);
+    if(domElements.length === 0) {
       throw new Error("bindDomEvent ERROR: Selector " + domSelector + " does not exist");
     }
-     forEach(domElements, function(domElement) {
+
+    forEach(domElements, function(domElement) {
       domElement.addEventListener(eventType, callback);
       return () => domElement.removeEventListener(eventType, callback);
     });
+
  }
