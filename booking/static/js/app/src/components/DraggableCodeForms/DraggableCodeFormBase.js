77a9d67a9ccc5e21c8bd769caf88605f8758ad2b
KC Soderstrom <soderstromkc@gmail.com>
2018-09-27 18:37:27 -0700

    Use DroppableContainer and dnd helpers for code form base

--- booking/static/js/app/src/components/DraggableCodeForms/DraggableCodeFormBase.js
+++ booking/static/js/app/src/components/DraggableCodeForms/DraggableCodeFormBase.js
@@ -1,5 +1,4 @@
 import React, { Component, Fragment } from 'react';
-import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import autoBind from 'react-autobind';
 import Grid from '@material-ui/core/Grid';
 import _ from 'lodash';
@@ -9,8 +8,16 @@ import DraggableCodeInputs from './DraggableCodeInputs';
 import HelpSection from './HelpSection';
 import TabbedList from '../HOComponents/TabbedList';
 import { Display1 } from '../typography';
+import DroppableContainer from '../DroppableContainer';
+import {
+  getCreatedItems,
+  getUpdatedItems,
+  getDestroyedItems,
+  removeItemFromForm,
+} from '../../helpers/dragAndDropHelpers';
 
-class DraggableCodeFormBase extends Component {
+// A base for editing models with a `code` attribute that needs a textarea
+class CodeFormBase extends Component {
   constructor(props) {
     super(props);
     autoBind(this);
@@ -25,45 +32,11 @@ class DraggableCodeFormBase extends Component {
     getArtistItems({artistId: profile.id});
   }
 
-  removeItemFromForm(order) {
-    const { currentValues, change, itemName } = this.props;
-    let remainingItems = _.clone(currentValues[itemName]);
-    // Leave blank in form to prevent popping
-    remainingItems[order] = { order };
-    change(itemName, remainingItems);
-  }
-
-  getUpdatedItems(currentItems) {
-    const { initialValues, itemName } = this.props;
-    const initialItems = initialValues[itemName];
-
-    const initialItemsByID = _.keyBy(initialItems, 'id');
-
-    return _.filter(currentItems, (item) => {
-      // Items not yet on the server can't be updated.
-      if (!item.id) return false;
-
-      const initialState = initialItemsByID[item.id];
-      const codeWasChanged = item.code !== initialState.code;
-      const orderWasChanged = item.order !== initialState.order;
-      return codeWasChanged || orderWasChanged;
-    });
-  }
-
-  getCreatedItems(currentItems) {
-    return _.filter(currentItems, item => !item.id);
-  }
-
-  getDestroyedItems(currentItems) {
-    const { initialValues, itemName } = this.props;
-    const initialItems = initialValues[itemName];
-
-    const currentItemsByID = _.keyBy(currentItems, 'id');
-
-    return _.filter(initialItems, (item) => {
-      // Items not yet on the server can't be destroyed.
-      return !!item.id && !_.has(currentItemsByID, item.id);
-    });
+  // TODO: could stand to clarify whether this index is the `id` or `order`
+  removeItemFromForm(itemIndex) {
+    const {currentValues, change, itemName} = this.props;
+    const currentItems = currentValues[itemName];
+    removeItemFromForm({currentItems, change, itemIndex, itemName});
   }
 
   getItemsWithCodes() {
@@ -81,15 +54,22 @@ class DraggableCodeFormBase extends Component {
       createArtistItem,
       destroyArtistItem,
       closeDialog,
+      initialValues,
+      itemName,
     } = this.props;
 
     // Treat items with no value in the `code` field as absent,
     // and ensure no gaps in `order` attributes.
-    const itemsWithCodes = this.getItemsWithCodes();
+    const currentItems = this.getItemsWithCodes();
+    const initialItems = initialValues[itemName];
 
-    const itemsToUpdate = this.getUpdatedItems(itemsWithCodes);
-    const itemsToCreate = this.getCreatedItems(itemsWithCodes);
-    const itemsToDestroy = this.getDestroyedItems(itemsWithCodes);
+    const itemsToUpdate = getUpdatedItems({
+      currentItems,
+      initialItems,
+      updatableFields: ['order', 'code'],
+    });
+    const itemsToCreate = getCreatedItems({currentItems});
+    const itemsToDestroy = getDestroyedItems({currentItems, initialItems});
 
     const updateRequests = _.map(itemsToUpdate, (item) => {
       updateArtistItem({
@@ -121,29 +101,6 @@ class DraggableCodeFormBase extends Component {
     Promise.all(requests).then(closeDialog);
   }
 
-  getReorderedItems(itemsFromForm, startIndex, endIndex) {
-    let reorderedItems = Array.from(itemsFromForm);
-    const [displacedItem] = reorderedItems.splice(startIndex, 1);
-    reorderedItems.splice(endIndex, 0, displacedItem);
-    reorderedItems = _.map(reorderedItems, (v, idx) => {return {...v, order: idx}});
-    return reorderedItems;
-  };
-
-  onDragEnd(result) {
-    const { currentValues, change, itemName } = this.props;
-
-    // Dropped outside the list
-    if (!result.destination) {
-      return;
-    }
-
-    change(itemName, this.getReorderedItems(
-      currentValues[itemName],
-      result.source.index,
-      result.destination.index,
-    ));
-  }
-
   ensureBlankInputAvailable() {
     const { currentValues, change, itemName } = this.props;
     const lastItemHasCode = !!(_.last(currentValues[itemName]) || {}).code;
@@ -156,58 +113,62 @@ class DraggableCodeFormBase extends Component {
 
   render() {
     const {
-      currentValues,
-      closeDialog,
-      submitting,
       handleSubmit,
+      provided,
       classes,
-      submitSucceeded,
-      width,
+      currentValues,
       itemName,
       copy,
+      width,
+      closeDialog,
+      submitting,
+      submitSucceeded,
     } = this.props;
 
     this.ensureBlankInputAvailable();
 
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
-        <Droppable droppableId={`${itemName}-edit-form`}>
-          {(provided, snapshot) => (
-            <div className={classes.container}>
-              <Grid container spacing={24}>
-                <TabbedList
-                    classes={classes}
-                    tabNames={['embed', 'help']} >
-                  <Display1>{copy.title}</Display1>
+      <Grid container spacing={24}>
+        <TabbedList
+            classes={classes}
+            tabNames={['embed', 'help']} >
+          <Display1>{copy.title}</Display1>
 
-                  <Fragment>
-                    <form onSubmit={handleSubmit(this.submit)} ref={provided.innerRef} className={classes.tabBody}>
-                      <DraggableCodeInputs
-                          items={currentValues[itemName]}
-                          itemName={itemName}
-                          placeholder={copy.inputPlaceholder}
-                          classes={classes}
-                          width={width}
-                          remove={this.removeItemFromForm} />
-                    </form>
-                    <CancelConfirm
-                        onClickCancel={closeDialog}
-                        onClickConfirm={this.submit}
-                        isLoading={submitting}
-                        success={submitSucceeded}
-                        isContainer={false} />
-                  </Fragment>
+          <Fragment>
+            <form
+                onSubmit={handleSubmit(this.submit)}
+                ref={provided.innerRef}
+                className={classes.tabBody} >
+              <DraggableCodeInputs
+                  items={currentValues[itemName]}
+                  itemName={itemName}
+                  placeholder={copy.inputPlaceholder}
+                  classes={classes}
+                  width={width}
+                  remove={this.removeItemFromForm} />
+            </form>
+            <CancelConfirm
+                onClickCancel={closeDialog}
+                onClickConfirm={this.submit}
+                isLoading={submitting}
+                success={submitSucceeded}
+                isContainer={false} />
+          </Fragment>
 
-                  <HelpSection className={classes.tabBody} classes={classes} helpCopyRows={copy.helpRows} title={copy.helpSectionTitle} />
+          <HelpSection
+              className={classes.tabBody}
+              classes={classes}
+              helpCopyRows={copy.helpRows}
+              itle={copy.helpSectionTitle} />
 
-                </TabbedList>
-              </Grid>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
+        </TabbedList>
+      </Grid>
     );
   }
 }
+
+const DraggableCodeFormBase = (props) => (
+  <DroppableContainer {...props} WrappedComponent={CodeFormBase} />
+);
 
 export default DraggableCodeFormBase;
