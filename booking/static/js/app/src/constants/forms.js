5c7c15c76e8d6882b44df309a3424fa5f09e4632
KC Soderstrom <soderstromkc@gmail.com>
2018-09-30 17:45:42 -0700

    Add redux const for photo edit form

--- booking/static/js/app/src/constants/forms.js
+++ booking/static/js/app/src/constants/forms.js
@@ -1,3 +1,4 @@
 export const EDIT_BASIC_INFO = 'EDIT_BASIC_INFO';
-export const EDIT_VIDEOS = 'EDIT_VIDEOS';
 export const EDIT_AUDIOS = 'EDIT_AUDIOS';
+export const EDIT_PHOTOS = 'EDIT_PHOTOS';
+export const EDIT_VIDEOS = 'EDIT_VIDEOS';
