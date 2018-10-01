00a079b5017065bbc7f1f1c76f7d904417075b96
KC Soderstrom <soderstromkc@gmail.com>
2018-09-30 17:43:01 -0700

    Add helper for getting thumbnail-sized image from cloudinary

--- booking/static/js/app/src/helpers/imageHelpers.js
+++ booking/static/js/app/src/helpers/imageHelpers.js
@@ -4,3 +4,10 @@ export const getMaxSizeImageURL = (imageURL, {maxWidth, maxHeight}) => (
   // TODO: surely there is a more legible way to do this
   imageURL.replace(/upload\/()v1/gi, `upload/w_${maxWidth},h_${maxHeight},c_limit/v1`)
 );
+
+// Gets a cloudinary URL for an image that will fill a 114px x 80px thumbnail window
+export const getThumbnailImageURL = (imageURL) => {
+    // TODO: surely there is a more legible way to do this
+  return imageURL.replace(/upload\/()v1/gi, 'upload/w_114,h_80,c_fill/v1');
+}
+
