13146ae7119b343a76233976c3bf71dcfdbd20a0
Chris Henry <henry.christopher@gmail.com>
2018-10-04 23:41:39 -0400

    new: Add more realistic example names

--- booking/tests/mommy_recipes.py
+++ booking/tests/mommy_recipes.py
@@ -6,6 +6,46 @@ from home.models import OpusUser
 
 import random
 
+videos = [
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/d6rxGmvQPLU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tt4j5t1YA-E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/i6sTatKAXt0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/9osn09WPv10" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
+]
+
+venue_names = [
+    'Cafe Wha?',
+    'The Cavern',
+    'Pianos',
+    'CBGB',
+    'Rockwood Music Hall',
+    'Arlene\'s Grocery',
+    'Elsewhere',
+    'Glasslands',
+    'The Room Next Door',
+    'KGB Bar',
+]
+
+rocker_first_name = [
+    'thor',
+    'jim',
+    'cody',
+    'brock',
+    'steven'
+]
+
+rocker_last_name = [
+    'henrikson',
+    'stark',
+    'morrison',
+    'turner',
+    'tyler'
+]
+
+def random_selection(list_options):
+    return random.choice(list_options)
+
 
 admin_user_recipe = Recipe(
     OpusUser,
@@ -21,11 +61,13 @@ user_musician_recipe = Recipe(
 musician_recipe = Recipe(
     Musician,
     user=foreign_key(user_musician_recipe),
+    stage_name="{} {}".format(random_selection(rocker_last_name), random_selection(rocker_last_name)),
     slug=seq("slug")
 )
 
 venue_recipe = Recipe(
-    Venue
+    Venue,
+    title=random_selection(venue_names)
 )
 
 event_recipe = Recipe(
@@ -39,19 +81,8 @@ slot_recipe = Recipe(
     musician=foreign_key(musician_recipe)
 )
 
-videos = [
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/d6rxGmvQPLU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tt4j5t1YA-E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/i6sTatKAXt0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
-    '<iframe width="560" height="315" src="https://www.youtube.com/embed/9osn09WPv10" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>'
-]
-
-def random_video():
-    return random.choice(videos)
-
 video_recipe = Recipe(
     MusicianVideo,
     musician=foreign_key(musician_recipe),
-    code=random_video()
+    code=random_selection(videos)
 )
