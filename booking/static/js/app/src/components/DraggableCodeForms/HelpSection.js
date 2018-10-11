bb1693508322b72df484eabb131d751a2154bd22
KC Soderstrom <soderstromkc@gmail.com>
2018-10-11 16:28:58 -0700

    Fix react warning in help section

--- booking/static/js/app/src/components/DraggableCodeForms/HelpSection.js
+++ booking/static/js/app/src/components/DraggableCodeForms/HelpSection.js
@@ -8,8 +8,8 @@ const HelpSection = (props) => {
   return <Grid container direction="column" className={className || ''} spacing={24}>
     <Grid item xs={12} sm={8} md={8} lg={8} style={{fontWeight: 'bold'}}>{title}</Grid>
     {
-      _.map(helpCopyRows, row => {
-        return <Grid item container direction="row">
+      _.map(helpCopyRows, (row, idx) => {
+        return <Grid item container direction="row" key={`help-section-row-${idx}`}>
           <Grid item xs={12} sm={6} md={6} lg={6} className={classes.helpScreenshotContainer}>{row[0]}</Grid>
           <Grid item xs={12} sm={6} md={6} lg={6} className={classes.helpTextContainer}>{row[1]}</Grid>
         </Grid>;
