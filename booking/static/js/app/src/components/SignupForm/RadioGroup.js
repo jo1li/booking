64daa56eb5454d931f356e0c6bed0c493bf36bde
alandickinson <patrick.alan@gmail.com>
2018-10-13 10:36:28 -0400

    add: wrap radiogroup mui element for use with redux

---
+++ booking/static/js/app/src/components/SignupForm/RadioGroup.js
@@ -0,0 +1,35 @@
+import FormHelperText from '@material-ui/core/FormHelperText';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import React, { Fragment } from 'react';
+
+const toString = (value) => {
+  if (value || typeof value === 'number') {
+    return value.toString();
+  }
+  return String();
+};
+
+const RadioGroupWithReduxForm = ({
+  input: { onChange, value, ...inputProps },
+  meta: { touched, error },
+  ...props
+}) => (
+  <Fragment>
+    <RadioGroup
+      {...inputProps}
+      {...props}
+      value={toString(value)}
+      onChange={(event, isInputChecked) => {
+        onChange(event, isInputChecked);
+      }}
+    />
+    {touched
+      && error && (
+        <FormHelperText className="x-error" error>
+          {error}
+        </FormHelperText>
+    )}
+  </Fragment>
+);
+
+export default RadioGroupWithReduxForm;
