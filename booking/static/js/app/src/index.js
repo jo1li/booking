045b31f57f8b033e8e9465fadc30da12fd2fb307
Chris Henry <henry.christopher@gmail.com>
2018-10-14 15:19:36 -0400

    fix: bad merge

--- booking/static/js/app/src/index.js
+++ booking/static/js/app/src/index.js
@@ -1,8 +1,20 @@
 import render_artist_profile_page from './pages/artist_profile';
 import render_artist_signup_page from './pages/artist_signup';
 import { ARTIST_PROFILE, ARTIST_SIGNUP } from './constants';
+
+import RenderFromDomNode from './renderFromDomNode';
+import NavigationBar from './components/nav/NavigationBar';
+
 import _ from 'lodash';
 
+
+// Global components
+RenderFromDomNode({
+    Component: NavigationBar,
+    node: 'main-navigation-bar',
+})
+
+// Page level components
 const RENDERERS_BY_PAGE = {
     [ARTIST_PROFILE]: render_artist_profile_page,
     [ARTIST_SIGNUP]: render_artist_signup_page,
