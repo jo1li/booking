a05fd04342060f945ec8004d3c7a271080ec3a11
KC Soderstrom <soderstromkc@gmail.com>
2018-10-18 15:47:33 -0700

    Start pulling shared edit form modal styles into their own module, with new modal sizing specs

--- booking/static/js/app/src/components/EditBioForm/styles.js
+++ booking/static/js/app/src/components/EditBioForm/styles.js
@@ -1,16 +1,7 @@
-// TODO: Share more of these styles between the modals
+import editModalStyles from '../../sharedStyles/editModalStyles';
+
 const styles = theme => ({
-  container: {
-    maxWidth: '100%',
-    width: '100%',
-    height: '100%',
-    padding: '20px',
-    paddingTop: '40px',
-    paddingBottom: '40px',
-  },
-  caption: {
-    color: theme.palette.grey['A400'],
-  },
+  ...editModalStyles(theme),
   captionTop: {
     margin: '0 12px',
     padding: '12px 0px 5px!important',
