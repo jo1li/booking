711a5a623d65a9cd684545697f33680c8312342e
KC Soderstrom <soderstromkc@gmail.com>
2018-10-18 14:57:48 -0700

    Update user edit form markup and styles to affix cancel-confirm to bottom of modal and title to top

--- booking/static/js/app/src/components/UserEditForm/styles.js
+++ booking/static/js/app/src/components/UserEditForm/styles.js
@@ -7,13 +7,11 @@ const styles = theme => ({
     height: '100%',
     padding: '20px',
     paddingTop: '40px',
-    paddingBottom: '40px',
   },
   caption: {
     color: theme.palette.grey['A400'],
   },
   captionTop: {
-    margin: '0 12px',
     padding: '12px 0px 5px!important',
     color: theme.palette.grey['A400'],
     borderBottom: `1px solid ${theme.palette.grey[200]}`,
@@ -26,6 +24,25 @@ const styles = theme => ({
       marginLeft: '0px',
     }
   },
+
+  // TODO: might want to move into a component
+  footer: {
+    borderTop: `1px solid ${theme.palette.grey[200]}`,
+    padding: '20px 0 12px',
+  },
+  withFooter: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  fixedHeight: {
+    flex: 0,
+  },
+  aboveFooter: {
+    padding: '12px 1px',
+    marginBottom: '12px',
+    overflowY: 'scroll',
+  },
+
 });
 
 export default styles;
