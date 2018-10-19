8b08c4c2811b39facecba0667dccbf94a833eaad
KC Soderstrom <soderstromkc@gmail.com>
2018-10-18 17:13:02 -0700

    Make cover photo focusable

--- booking/static/js/app/src/components/CoverPhoto.js
+++ booking/static/js/app/src/components/CoverPhoto.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 const CoverPhoto = (props) => {
   const { photos, profile } = props;
   const coverPhoto = photos[profile.image_hero_id];
   if(!coverPhoto) return <div id="cover-photo" />;
 
-  return <div id="cover-photo" style={{
-    backgroundImage: `url(${coverPhoto.image})`,
-  }} />
+  return (
+    <div
+      id="cover-photo"
+      tabIndex={0}
+      style={{
+        backgroundImage: `url(${coverPhoto.image})`,
+    }} />
+  );
 }
 
 const mapStateToProps = (state, props) => ({
