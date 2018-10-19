aef14d57924adbd481dd84c7061998ac9213e38c
KC Soderstrom <soderstromkc@gmail.com>
2018-10-18 17:15:49 -0700

    Prevent photo modal button from changing opacity on hover

--- booking/static/js/app/src/components/PhotoModalButton.js
+++ booking/static/js/app/src/components/PhotoModalButton.js
@@ -7,7 +7,7 @@ import CONFIGS from '../configs';
 const PhotoModalButton = (props) => (
   <a
       href="#"
-      className={`edit photo-modal-button ${_.size(props.photos) ? '' : 'hidden'}`} >
+      className={`edit no-transluscent-hover photo-modal-button ${_.size(props.photos) ? '' : 'hidden'}`} >
     <img src={`${CONFIGS.IMAGES_URL}/camera.svg`} alt="camera icon" />
     <span>{_.size(props.photos)}</span>
   </a>
