63b7947963d6ccc8665e155a419b4042f4e82dd7
alandickinson <patrick.alan@gmail.com>
2018-10-23 10:57:25 -0400

    adds a select field wrapper that works with multi-select

---
+++ booking/static/js/app/src/components/SignupForm/SelectField.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+
+const noop = () => {};
+
+const SelectField = ({ 
+  input, 
+  label, 
+  meta: { touched, error }, 
+  children, 
+  ...rest }) => (
+  <FormControl error={Boolean(touched && error)} fullWidth>
+    {label && <InputLabel>{label}</InputLabel>}
+    <Select
+      {...input}
+      onChange={event => input.onChange(event.target.value)}
+      onBlur={noop} // https://github.com/erikras/redux-form/issues/2768
+      value={input.value}
+      {...rest}
+    >
+      {children}
+    </Select>
+    {touched && error && <FormHelperText>{error}</FormHelperText>}
+  </FormControl>
+);
+
+export default SelectField;
