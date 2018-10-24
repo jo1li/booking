4b90af41a8560d7f12c062f6a2056cf1329cdd76
alandickinson <patrick.alan@gmail.com>
2018-10-24 12:20:03 -0400

    chg: switch to stateful component to support refs and getRenderedComponent()

--- booking/static/js/app/src/components/SignupForm/SelectField.js
+++ booking/static/js/app/src/components/SignupForm/SelectField.js
@@ -6,25 +6,26 @@ import Select from '@material-ui/core/Select';
 
 const noop = () => {};
 
-const SelectField = ({ 
-  input, 
-  label, 
-  meta: { touched, error }, 
-  children, 
-  ...rest }) => (
-  <FormControl error={Boolean(touched && error)} fullWidth>
-    {label && <InputLabel>{label}</InputLabel>}
-    <Select
-      {...input}
-      onChange={event => input.onChange(event.target.value)}
-      onBlur={noop} // https://github.com/erikras/redux-form/issues/2768
-      value={input.value}
-      {...rest}
-    >
-      {children}
-    </Select>
-    {touched && error && <FormHelperText>{error}</FormHelperText>}
-  </FormControl>
-);
+class SelectField extends React.Component {
+  render(props) {
+    const { input, label, meta: { touched, error }, children, ...rest } = this.props
+    return (
+      <FormControl error={Boolean(touched && error)} fullWidth>
+        {label && <InputLabel>{label}</InputLabel>}
+        <Select
+          {...input}
+          onChange={event => input.onChange(event.target.value)}
+          onBlur={noop} // https://github.com/erikras/redux-form/issues/2768
+          value={input.value}
+          {...rest}
+        >
+          {children}
+        </Select>
+        {touched && error && <FormHelperText>{error}</FormHelperText>}
+      </FormControl>     
+    )
+  }
+
+}
 
 export default SelectField;
