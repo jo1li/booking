adebe75bfb262410b51b738a1ea62f70f9c93f2b
Chris Henry <henry.christopher@gmail.com>
2018-10-28 18:16:07 -0400

    fix: define DATABASES object

--- booking/booking/settings/stage-aws.py
+++ booking/booking/settings/stage-aws.py
@@ -3,8 +3,9 @@ from .base import *
 import urllib
 import dj_database_url
 
+DATABASES = {}
 DATABASES['default'] = dj_database_url.parse(
-    urllib.parse.quote_plus('mysql://booking_stage:A;vj,ayDF7hUJ2V$V[}9yZPx@booking-stage.cuo6krbubjof.us-east-2.rds.amazonaws.com:3306/booking_stage'),
+    "mysql://booking_stage:{}@booking-stage.cuo6krbubjof.us-east-2.rds.amazonaws.com:3306/booking_stage".format(urllib.parse.quote_plus("A;vj,ayDF7hUJ2V$V[}9yZPx")),
     conn_max_age=0
 )
 
