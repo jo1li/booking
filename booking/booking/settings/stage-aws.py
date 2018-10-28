ed3b8a6b07105988cf4437a40bd7b484193340ea
Chris Henry <henry.christopher@gmail.com>
2018-10-28 17:58:37 -0400

    chg: URLencode MysQL URL

--- booking/booking/settings/stage-aws.py
+++ booking/booking/settings/stage-aws.py
@@ -1,11 +1,12 @@
 from .base import *
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import urllib
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.parse(
+    urllib.parse.quote_plus('mysql://booking_stage:A;vj,ayDF7hUJ2V$V[}9yZPx@booking-stage.cuo6krbubjof.us-east-2.rds.amazonaws.com:3306/booking_stage'),
+    conn_max_age=0
+)
 
 SITE_DOMAIN = 'booking-stage-1029564567.us-east-2.elb.amazonaws.com'
 ALLOWED_HOSTS = [
