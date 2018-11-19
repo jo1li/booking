6304c129a94534003b0e13824d9ff15f20fd1fa1
Chris Henry <henry.christopher@gmail.com>
2018-11-19 00:18:42 -0500

    Build fixes, take 2 (#127)
    * fix: Ensure new artists have the correct flags set on their OpusUser object
    
    * chg: Build improvements

--- infra/watch.py
+++ infra/watch.py
@@ -2,22 +2,32 @@
 
 import boto3
 from utils_git import get_git_sha
+from utils import _stack_exists
+
+import argparse
 
 if __name__ == "__main__":
-    print("here")
 
+    parser = argparse.ArgumentParser(description="Check _stack_exists")
+    parser.add_argument("--stack-name", help="Stack name to check", default=None,
+                        action="store")
+    args = parser.parse_args()
 
-    client = boto3.client('ecr')
+    print(args.stack_name)
 
-    r = client.describe_images(
-        repositoryName='booking',
-        imageIds=[
-            {
-                'imageTag': get_git_sha()
-            },
-        ],
-    )
+    print(_stack_exists(args.stack_name))
 
-    import pprint
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(r)
+    # client = boto3.client('ecr')
+
+    # r = client.describe_images(
+    #     repositoryName='booking',
+    #     imageIds=[
+    #         {
+    #             'imageTag': get_git_sha()
+    #         },
+    #     ],
+    # )
+
+    # import pprint
+    # pp = pprint.PrettyPrinter(indent=4)
+    # pp.pprint(r)
