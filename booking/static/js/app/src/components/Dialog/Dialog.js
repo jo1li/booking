e2da2b64a3de3ed44ae9b0b25f8c9f48e239c926
Chris Henry <henry.christopher@gmail.com>
2018-11-27 23:59:46 -0500

    fix: Avoid tons of warnings, by only passing relevant prop

--- booking/static/js/app/src/components/Dialog/Dialog.js
+++ booking/static/js/app/src/components/Dialog/Dialog.js
@@ -51,7 +51,7 @@ const DialogStateManager = Dialog => WrappedComponent => {
                     <Dialog
                         isOpen={this.state.isOpen}
                         close={this.closeDialog}
-                        {...this.props}
+                        paperProps={this.props.paperProps}
                     >
                         { content }
                     </Dialog>
