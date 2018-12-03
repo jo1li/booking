fc85c97e8450c9c4d4f6cd07fa20f94fbfcdcdb8
Chris Henry <henry.christopher@gmail.com>
2018-12-02 22:26:04 -0500

    Settings and Forgot Password (#128)
    * fix: Widen login form to accomodate reasonable email lengths
    
    * fix: no one wants rounded corners on inputs
    
    * new: Fully implement password change / settings
    
    * new: Fix passwor reset flow
    
    * new: Template for failed password reset token
    
    * new: Add a help link to settings
    
    * fix: Set opacity to 1 so buttons look enabled

--- booking/home/hooks.py
+++ booking/home/hooks.py
@@ -8,6 +8,7 @@ from account.hooks import AccountDefaultHookSet
 
 class AccountHookSet(AccountDefaultHookSet):
 
+
     def send_confirmation_email(self, to, ctx):
 
         # TODO: this won't work in temp branch builds
@@ -30,4 +31,29 @@ class AccountHookSet(AccountDefaultHookSet):
         email.send(fail_silently=False)
 
 
+    def send_password_reset_email(self, to, ctx):
+
+        # TODO: this won't work in temp branch builds
+        protocol = getattr(settings, "DEFAULT_HTTP_PROTOCOL", "http")
+        ctx['password_reset_url'] = "{0}://{1}{2}".format(
+            protocol,
+            settings.SITE_DOMAIN,
+            ctx['password_reset_url'].replace('http://example.com', '')
+        )
+        print(ctx)
+
+        html_content = render_to_string("account/email/password_reset.html", ctx)
+
+        email = EmailMultiAlternatives(
+            'Change your Opus password',
+            "Change your password here: {}".format(ctx['password_reset_url']),
+            'info@opuslive.io',
+            [to],
+        )
+        email.attach_alternative(html_content, "text/html")
+        email.send(fail_silently=False)
+
+
+    def send_password_change_email(self, to, ctx):
+        pass
 
