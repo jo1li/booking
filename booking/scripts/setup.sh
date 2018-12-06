897b9a8c8cd94b93981c7cf77d5eef6ae6089566
Chris Henry <henry.christopher@gmail.com>
2018-12-05 22:57:28 -0500

    new: Compile sass during startup

--- booking/scripts/setup.sh
+++ booking/scripts/setup.sh
@@ -27,6 +27,7 @@ python manage.py initial_tags
 # Move static assets into place
 if [[ $* != *--skip-collect* ]]
 then
+    python manage.py compilescss
     python manage.py collectstatic --noinput -i node_modules
 else
     echo "Skipping collect 4 speedzz!!!1!"
