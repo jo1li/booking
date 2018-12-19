5159b4231c1acd3677c86cf3dfbf8cf58e9d920b
KC Soderstrom <soderstromkc@gmail.com>
2018-12-18 22:47:46 -0800

    Allow larger images in photo carousel to utilize full-screen design

--- booking/static/js/app/src/constants/carousels.js
+++ booking/static/js/app/src/constants/carousels.js
@@ -1,4 +1,4 @@
 export const VIDEO_ASPECT_RATIO = 56;
 export const AUDIO_ASPECT_RATIO = 72;
-export const MAX_IMAGE_WIDTH = 620;
-export const MAX_IMAGE_HEIGHT = Math.round(MAX_IMAGE_WIDTH * IMAGE_ASPECT_RATIO / 100);
+export const MAX_IMAGE_WIDTH = 1920;
+export const MAX_IMAGE_HEIGHT = 1200;
