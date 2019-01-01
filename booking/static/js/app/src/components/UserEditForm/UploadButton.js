aa836975179521ecb978ab3d1a1be75bb2c8431c
Zachary Stallings <zakstal@users.noreply.github.com>
2018-12-31 18:15:53 -0800

    Image upload mobile (#150)
    # Changelog
    [mobile-trying-to-upload-an-avatar-through-the-edit-bio-modal-doesnt-work](https://trello.com/c/pT6rZH3u/268-high-priority-on-mobile-trying-to-upload-an-avatar-through-the-edit-bio-modal-doesnt-work)
    
    Instead of spending too much time on trying to get a `button` element working I opted for creating a new component with similar styles. Using a `div` element the on change event works on both ios and chrome. 
    
    ## New
    - Add `components/UserEditForm/UploadButton.js`
    
    ## Change
    -  `components/UserEditForm/UserEditForm.js` to use the new `UploadButton` component.
    
    https://github.com/OpusApps/booking/pull/150

---
+++ booking/static/js/app/src/components/UserEditForm/UploadButton.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { CloudUpload } from '../icons';
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = theme => ({
+    container: {
+        border: `solid 1px ${theme.palette.secondary.main}`,
+        outline: 'none',
+        borderColor: theme.palette.secondary.main,
+        borderRadius: '100%',
+        color: theme.palette.secondary.main,
+        padding: '8px 9px',
+        fontSize: '0.8125rem',
+        width: '44px',
+        height: '44px',
+        outline: 'none',
+        minWidth: '44px',
+        minHeight: '44px',
+        borderColor: theme.palette.secondary.main,
+        borderRadius: '100%',
+        '&:hover': {
+            border: `1px solid ${theme.palette.secondary.main}`,
+            backgroundColor: 'rgba(0, 185, 209, 0.08)',
+        }
+    }
+})
+
+const FakeUploadButton = ({ classes }) =>
+    <div className={classes.container}>
+         <CloudUpload />
+    </div>
+
+export default withStyles(styles)(FakeUploadButton);
+
+/*const FakeUploadButton = () =>
+    <div className="MuiButtonBase-root-87 MuiButton-root-375 MuiButton-outlined-383 MuiButton-outlinedSecondary-385 MuiButton-sizeSmall-398 ContainedButtons-button-374 FabButton-button-401 FabButton-button-401 undefined">
+         <CloudUpload />
+    </div>
+
+export default withStyles(styles)(FakeUploadButton);*/
