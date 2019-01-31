0884182e8fc840ea40e735032af701db4bc97d1d
KC Soderstrom <soderstromkc@gmail.com>
2019-01-30 19:01:22 -0800

    Enable resizing scaled elements after initial render

--- booking/static/js/app/src/components/ScaledElement.js
+++ booking/static/js/app/src/components/ScaledElement.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import autobind from 'react-autobind';
 import { withStateHandlers } from 'recompose';
 import propTypes from 'prop-types';
 
 class ScaledElement extends Component {
-    getDimensions() {
+    componentWillMount() {
+        autobind(this);
+        this.setState({});
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateContainerDimensions, 0);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateContainerDimensions);
+    }
+
+
+    // This state won't be used, except to trigger re-render if changed.
+    // There isn't a good way to use this as a value for rendering, because
+    // the container isn't set until after the initial render.
+    updateContainerDimensions() {
         if (!this.container) {
+            return;
+        }
+
+        this.setState({
+            containerWidth: this.container.offsetWidth,
+            containerHeight: this.container.offsetHeight,
+        });
+    }
+
+    getDimensions() {
+        if(!this.container) {
             return {};
         }
 
@@ -13,16 +42,18 @@ class ScaledElement extends Component {
         } = this.props;
 
         const width = this.container.offsetWidth;
+        const maxHeight = this.container.offsetHeight;
 
         return {
-            width,
-            height: width / ratio,
-        }
+            width: Math.min(width, maxHeight * ratio),
+            height: Math.min(maxHeight, width / ratio),
+        };
     }
 
     render() {
         const {
             className,
+            style,
             render,
         } = this.props;
 
