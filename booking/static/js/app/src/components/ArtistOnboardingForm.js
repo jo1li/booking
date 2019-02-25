8216c4d1eead1252f041587dac35cfd1ddd53030
KC Soderstrom <soderstromkc@gmail.com>
2019-02-24 21:46:28 -0800

    Remove unused validations

--- booking/static/js/app/src/components/ArtistOnboardingForm.js
+++ booking/static/js/app/src/components/ArtistOnboardingForm.js
@@ -58,68 +58,19 @@ class OnboardingForm extends Component {
   }
 
   submit = async (values) => {
-    // TODO: is any of this even used for anything?
     const { updateUserBio, musicianid, imageFile } = this.props;
 
     const genres = values.genres ? values.genres.map(g => `"${g.value}"`).join(",") : "";
     let data = { ...values, genres }
 
-    const errors = {}
-
-    // tagline validation
-    if(!data.bio_short) {
-      errors.bio_short = "This field is required.";
-    }
-
-    // genres validation
-    if(_.isEmpty(data.genres)) {
-      errors.genres = "Please select at least 1 genre.";
-    }
-
-    // url regex
-    const simple_url_regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/){1}[a-zA-Z0-9]+([\-\.]{1}[a-zA-Z0-9]+)*\.[a-zA-Z]{2,25}(:[0-9]{1,5})?(\/.*)?$/;
-
-    // website validation
-    if (data.website && !simple_url_regex.test(data.website)) {
-      errors.website = "This link looks invalid. Please make sure the link starts with http:// or https://";
-    }
-
-    const urlerror = "This link looks invalid. Please check for typos and make sure the link starts with https://";
-
-    // facebook validation
-    if (data.facebook && data.facebook.toLowerCase().indexOf('facebook.com') === -1) {
-      errors.facebook = "Please use a Facebook link here."
-    } else if (data.facebook && !validateURL(data.facebook)) {
-      errors.facebook = urlerror;
-    }
-
-    // instagram validation
-    if (data.instagram && data.instagram.toLowerCase().indexOf('instagram.com') === -1) {
-      errors.instagram = "Please use an Instagram link here."
-    } else if (data.instagram && !validateURL(data.instagram)) {
-      errors.instagram = urlerror;
-    }
-
-    // spotify validation
-    if (data.spotify && data.spotify.toLowerCase().indexOf('spotify.com') === -1) {
-      errors.spotify = "Please use a Spotify link here."
-    } else if (data.spotify && !validateURL(data.spotify)) {
-      errors.spotify = urlerror;
-    }
-
-    if(Object.keys(errors).length === 0) {
-       try {
-          const res = await updateUserBio(data, musicianid);
-
-          if (res.data.url_fq) {
-            window.location.href = res.data.url_fq
-          }
-        } catch(errors) {
-          console.error('Error in OnboardingForm submit', errors);
-        };
-    } else {
-      throw new SubmissionError(errors);
-    }
+    try {
+      const res = await updateUserBio(data, musicianid);
+      if (res.data.url_fq) {
+        window.location.href = res.data.url_fq;
+      }
+    } catch(errors) {
+      console.error('Error in OnboardingForm submit', errors);
+    };
 
   }
 
